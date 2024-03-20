12789d5575257061fb50d9795bf4eb66fd845110
peejay <peejay.beltran12@gmail.com>
2024-03-20 16:49:53 +0800

    fifth commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage{
+  myArray = [];
+  nextnumber: number = 0;
+  loadingText = "Loading...";
+  isLoading = false;
+  isHide = true;
+
+
+  constructor(private router: Router, private dataService: DataService, private cdr: ChangeDetectorRef) {
+    console.log(this.dataService.canProceed);
+  }
+  
+  async showObject(){
+    try {
+      this.isLoading = true;
+      if (this.isLoading == true) {
+        this.isHide = false;
+        this.myArray = await this.dataService.fetchData1();
+        console.log(this.myArray);
+      }
+      this.isHide = true;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  clickUnauthenticate(){
+    this.dataService.canProceed = false;
+    console.log('Successfully Unauthenticate!');
+    this.isHide = false;
+  }
+
+  clickAuthenticate() {
+    this.dataService.canProceed = true;
+    console.log('Successfully Authenticate!');
+    this.isHide = false;
+  }
+
+  async addObject(){
+    this.myArray = await this.dataService.fetchData1();
+    this.nextnumber = this.myArray.length + 1;
+    this.dataService.addToMyArray(this.nextnumber);
+  }
 
 }
